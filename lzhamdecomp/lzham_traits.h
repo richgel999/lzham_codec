c1dfe50b723a1410d466e63123ad42a9089b0e8a
Bryan Woods <woods.bryan@gmail.com>
2015-03-23 14:19:14 -0700

    OS X fixes
    Tested with:
    
      - Apple LLVM version 6.0 (clang-600.0.57) (based on LLVM 3.5svn)
      - gcc version 4.7.2 (Debian 4.7.2-5)

--- lzhamdecomp/lzham_traits.h
+++ lzhamdecomp/lzham_traits.h
@@ -67,7 +67,9 @@ namespace lzham
    // Defines type Q as bitwise copyable.
 #define LZHAM_DEFINE_BITWISE_COPYABLE(Q) template<> struct bitwise_copyable<Q> { enum { cFlag = true }; };
 
-#if defined(__APPLE__) || defined(__NetBSD__)
+#if defined(__APPLE__)
+   #define LZHAM_IS_POD(T) std::is_pod<T>::value
+#elif defined(__NetBSD__)
    #define LZHAM_IS_POD(T) std::__is_pod<T>::__value
 #else
    #define LZHAM_IS_POD(T) __is_pod(T)
